21f2c7dabe4e1849c106df589db14071d0d313d6
Cesar Caus <cesarcaustec@gmail.com>
2024-03-13 15:14:27 -0300

    Atualização interface-01

--- src/app/components/interfaces/glassmorphism-login-01/glassmorphism-login-01.component.ts
+++ src/app/components/interfaces/glassmorphism-login-01/glassmorphism-login-01.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'glassmorphism-login-01',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent,
+  RegisterComponent],
   templateUrl: './glassmorphism-login-01.component.html',
   styleUrl: './glassmorphism-login-01.component.scss'
 })
